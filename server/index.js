53674b562d04cadbf771f0de7e57e46b5f98ce32
archon21 <rwetmore3@gmail.com>
2018-09-21 15:09:23 -0400

    added a simple backend express server

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+
+const PORT = process.env.PORT || 8080
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+
+// app.use('/auth', require('./auth'))
+app.use('/api', require('./api'))
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found')
+      err.status = 404
+      next(err)
+    } else {
+      next()
+    }
+  })
+
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+});
+
+app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+
+
+app.listen(PORT, () => {
+  console.log(`Running server on PORT ${PORT}`);
+});
